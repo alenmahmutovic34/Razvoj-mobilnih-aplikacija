b14d5b613a2b054b50872d3d12be623468ff123e
alenmahmutovic34 <163427024+alenmahmutovic34@users.noreply.github.com>
2024-10-21 21:43:05 +0200

    Update Cifrograd.kt

--- LV1/Cifrograd.kt
+++ LV1/Cifrograd.kt
@@ -1,1 +1,21 @@
+fun getSneakyNumbers(nums: IntArray): IntArray {
+    var seen = mutableListOf<Int>()
+    var result = mutableListOf<Int>()
 
+    for(num in nums) {
+        if(seen.contains(num)) {
+            result.add(num)
+        }else {
+            seen.add(num)
+        }
+    }
+    return result.toIntArray()
+}
+
+fun main() {
+
+    val nums1 = intArrayOf(0,1,1,0)
+    val nums2 = intArrayOf(0,3,2,1,3,2)
+    println(getSneakyNumbers(nums1).joinToString())
+    println(getSneakyNumbers(nums2).joinToString())
+}
